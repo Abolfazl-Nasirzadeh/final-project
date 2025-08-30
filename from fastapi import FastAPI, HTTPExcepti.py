e5039fcc20beb9231ebf1de267104f429754f49f
Abolfazl Nasirzadeh <abolfazln5050@gmail.com>
2025-08-30 20:56:27 +0330

    hash of password

--- from fastapi import FastAPI, HTTPExcepti.py
+++ from fastapi import FastAPI, HTTPExcepti.py
@@ -52,3 +52,10 @@ class LoginResponse(BaseModel):
     token_type: str = "bearer"
     user: UserPublic
 
+def hash_password(plain_password: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(plain_password.encode("utf-8"), salt)
+    return hashed.decode("utf-8")
+
+def verify_password(plain_password: str, password_hash: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode("utf-8"), password_hash.encode("utf-8"))
